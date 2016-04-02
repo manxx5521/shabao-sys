1c3a4236b9186fdd64d59402d35e9825f49860b5
manxx <manxx5521@163.com>
2016-04-02 22:36:15 +0800

    修改声明

--- src/main/java/com/xiaoshabao/framework/wechat/api/wxmenu/model/WeixinButton.java
+++ src/main/java/com/xiaoshabao/framework/wechat/api/wxmenu/model/WeixinButton.java
@@ -1,17 +1,38 @@
 package com.xiaoshabao.framework.wechat.api.wxmenu.model;
 
 import java.util.List;
-
+/**
+ * 菜单列表实体类
+ */
 public class WeixinButton {
-
-	private String type;
-	
+	/**
+	 * 菜单名字
+	 */
 	private String name;
 	
-	private String key;
+	/**
+	 * 菜单类型
+	 * 使用{@link com.xiaoshabao.framework.wechat.api.wxmenu.MenuType}中的类型
+	 */
+	private String type;
 	
+	/**
+	 * 菜单的key,key为消息回调是的识别码。当菜单为click等类型时，并且本菜单为最末级菜单时有效。
+	 */
+	private String key;
+	/**
+	 * 点击菜单后跳转的URL。菜单为view时，并且本菜单为最末级菜单时有效。
+	 */
 	private String url;
 	
+	/**
+	 * 菜单的素材id,当菜单为media_id类型和view_limited必须、有用，本菜单为最末级级菜单时有效。
+	 */
+	private String media_id;
+	
+	/**
+	 * 子菜单，当有二级菜单时有效
+	 */
 	private List<WeixinButton> sub_button;
 
 	public String getType() {
@@ -52,6 +73,14 @@ public class WeixinButton {
 
 	public void setSub_button(List<WeixinButton> sub_button) {
 		this.sub_button = sub_button;
+	}
+
+	public String getMedia_id() {
+		return media_id;
+	}
+
+	public void setMedia_id(String media_id) {
+		this.media_id = media_id;
 	}
 	
 }
